c5a803efb4a090b58db745eb33350b7745669c77
Adrien Schaffner <adrien.schaffner@gmail.com>
2022-05-26 12:00:04 +0200

    profile view with teckel and resa

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,6 @@
 class ReservationsController < ApplicationController
+  before_action :set_params, only: :destroy
+
   def new
     @reservation = Reservation.new
   end
@@ -8,7 +10,16 @@ class ReservationsController < ApplicationController
     @reservation.save
   end
 
+  def destroy
+    @reservation.destroy
+    redirect_to profile_path
+  end
+
   private
+
+  def set_params
+    @reservation = Reservation.find(params[:id])
+  end
 
   def reservation_params
     params.require(:reservation).permit(:starting_date, :ending_date)
